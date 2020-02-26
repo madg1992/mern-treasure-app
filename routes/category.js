7acb95b390e88c97f102e950d2c26a79fa3643b9
Mark Gallardo <mrk_gllrd@yahoo.com>
2020-02-26 15:46:00 -0600

    added routes for Admins to update and delete categories

--- routes/category.js
+++ routes/category.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, categoryById, read } = require('../controllers/category');
+const { create, categoryById, read, update, remove, list } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/userAuth');
 const { userById } = require('../controllers/user');
 
 // route for admins to create a category
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
-// route for admins to fetch a category
-router.get('/category/:categoryId', read)
+// route to fetch a category
+router.get('/category/:categoryId', read);
 
+// route reserved for admins to update a category
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
 
+// route reserved for admins to delete a category
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+
+// route for fetching ALL categories
+router.get('/categories', list);
 
 // anytime there is 'userId' in the route param, then this middleware will run
 router.param('userId', userById);
